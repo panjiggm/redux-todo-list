eed0fadca2bb456f8b9a33c8e06bf1f376ba16b3
Panji Gumelar <panjiggm@gmail.com>
2019-02-13 14:42:49 +0700

    refactor rootreducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,4 @@
 import { createStore } from 'redux'
 import rooeReducer from './reducers'
 
-export default createStore(
-  rooeReducer,
-  +window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
+export default createStore(rooeReducer)
